aed3a479e06515e9591f4bca8ebe7aa696370e79
hyloosec-official-creator <hyloosec.official@gmail.com>
2026-05-30 10:30:16 +0530

    site is under maintancae

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
-import App from './App.jsx'
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-)
+// मेंटेनेंस मोड का वेरिएबल (या तो यहीं true/false करो या ENV यूज़ करो)
+const isMaintenance = import.meta.env.VITE_MAINTENANCE_MODE === 'true';; 
+
+if (isMaintenance) {
+  createRoot(document.getElementById('root')).render(
+    <div style={{
+    display: "flex",           // Flexbox ऑन किया
+    flexDirection: "column",   // चीजों को ऊपर-नीचे रखा
+    justifyContent: "center",  // वर्टिकली (ऊपर-नीचे) सेंटर
+    alignItems: "center",      // हॉरिजॉन्टली (बाएं-दाएं) सेंटर
+    height: "100vh",           // पूरी स्क्रीन की ऊंचाई
+    fontFamily: "sans-serif",
+    color: "#9a9a9a",
+    textAlign: "center"
+}}>
+    <h1>🛠️ Site Under Maintenance</h1>
+    <p>We are currently optimizing our infrastructure. We'll be back soon!</p>
+</div>
+  );
+} else {
+  createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
