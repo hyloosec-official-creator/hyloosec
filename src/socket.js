bdcbc70dee6c2c12d46ca839b4ed0b4403c25c78
hyloosec-official-creator <hyloosec.official@gmail.com>
2026-06-20 16:35:09 +0530

    this is the updated v1 of hyloosec v1

--- src/socket.js
+++ src/socket.js
@@ -1,19 +1,11 @@
 import { io } from "socket.io-client";
 
-// वही लॉजिक जो तुम्हारे API फाइल में है
-const USE_LOCAL_SERVER = false; 
-
-const SOCKET_URL = USE_LOCAL_SERVER 
-  ? "http://localhost:5000" 
-  : import.meta.env.VITE_MONGO_API_URL; // यहाँ अपनी प्रोडक्शन वाली URL डालें
-
-// Create the socket instance
-const socket = io(SOCKET_URL, {
-  timeout: 5000, // 5 सेकंड के बाद पक्का एरर दे
-  reconnectionAttempts: 3, // 3 बार के बाद हार मान ले
-  transports: ["polling", "websocket"],
-  autoConnect: false,
+const socket = io("https://node.hyloosec.online", {
+  path: "/socket.io/",
+  // इसे ["polling", "websocket"] पर सेट करो, ताकि अगर websocket फेल हो तो polling से काम चल जाए
+  transports: ["polling", "websocket"], 
   withCredentials: true,
+  autoConnect: false,
 });
 
 socket.io.on("error", (error) => {
