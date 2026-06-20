aa505daf8ea7e69c1f915388bf3fce2e7638db70
hyloosec-official-creator <hyloosec.official@gmail.com>
2026-06-20 16:15:06 +0530

    this is the updated v1 of hyloosec v1

--- src/socket.js
+++ src/socket.js
@@ -9,9 +9,19 @@ const SOCKET_URL = USE_LOCAL_SERVER
 
 // Create the socket instance
 const socket = io(SOCKET_URL, {
+  timeout: 5000, // 5 सेकंड के बाद पक्का एरर दे
+  reconnectionAttempts: 3, // 3 बार के बाद हार मान ले
   transports: ["polling", "websocket"],
   autoConnect: false,
   withCredentials: true,
+});
+
+socket.io.on("error", (error) => {
+  console.log("Socket Global Error:", error);
+});
+
+socket.on("connect_error", (err) => {
+  console.log("Connection Failed:", err.message);
 });
 
 export default socket;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true, // ये सारे console.log हटा देगा
-        drop_debugger: true,
-      },
-    },
-  },
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     minify: 'terser',
+//     terserOptions: {
+//       compress: {
+//         drop_console: true, // ये सारे console.log हटा देगा
+//         drop_debugger: true,
+//       },
+//     },
+//   },
+// })
