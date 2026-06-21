a17d926b895028ae0f5ec223f20691f11e5623a4
hyloosec-official-creator <hyloosec.official@gmail.com>
2026-06-21 13:11:11 +0530

    message security fix

--- vite.config.js
+++ vite.config.js
@@ -18,24 +18,24 @@ export default defineConfig({
     })
   ],
 
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true, // कंसोल लॉग्स को हटा देगा
-        drop_debugger: true,
-      },
-    },
-    // बंडल साइज को छोटा करने के लिए code splitting
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // सारी बड़ी लाइब्रेरीज़ (socket, react, आदि) को 'vendor' फाइल में अलग कर देगा
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 600, // वॉर्निंग लिमिट थोड़ी बढ़ा दी है
-  },
+  // build: {
+  //   minify: 'terser',
+  //   terserOptions: {
+  //     compress: {
+  //       drop_console: true, // कंसोल लॉग्स को हटा देगा
+  //       drop_debugger: true,
+  //     },
+  //   },
+  //   // बंडल साइज को छोटा करने के लिए code splitting
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           return 'vendor'; // सारी बड़ी लाइब्रेरीज़ (socket, react, आदि) को 'vendor' फाइल में अलग कर देगा
+  //         }
+  //       }
+  //     }
+  //   },
+  //   chunkSizeWarningLimit: 600, // वॉर्निंग लिमिट थोड़ी बढ़ा दी है
+  // },
 });
