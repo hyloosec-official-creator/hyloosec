4d67ecb838a41297d381495667ea8e3402510e54
hyloosec-official-creator <hyloosec.official@gmail.com>
2026-06-29 11:04:08 +0530

    fixing performance v3

--- src/assets/components/jsx-components/Home/SideBar.jsx
+++ src/assets/components/jsx-components/Home/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import logo from "../../../images/logo/logo.png";
 import { CiSearch, CiUser, CiChat1 } from "react-icons/ci";
