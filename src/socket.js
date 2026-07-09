f07053d95118f8c386145b8cc6cc7729a18bc033
hyloosec-official-creator <hyloosec.official@gmail.com>
2026-07-09 14:13:41 +0530

    about page fixing

--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,16 @@
 import { io } from "socket.io-client";
 
-// const socket = io("https://node.hyloosec.online", {
-//   path: "/socket.io/", // सर्वर के Server(server, { path: "/socket.io/" }) से मैच होना चाहिए
-//   transports: ["websocket"],
-//   secure: true
-// });
-
-const socket = io("http://localhost:5000", {
+const socket = io("https://node.hyloosec.online", {
   path: "/socket.io/", // सर्वर के Server(server, { path: "/socket.io/" }) से मैच होना चाहिए
   transports: ["websocket"],
   secure: true
 });
+
+// const socket = io("http://localhost:5000", {
+//   path: "/socket.io/", // सर्वर के Server(server, { path: "/socket.io/" }) से मैच होना चाहिए
+//   transports: ["websocket"],
+//   secure: true
+// });
 
 socket.on("connect", () => console.log("🚀 Socket Connected ID:", socket.id));
 socket.on("connect_error", (err) => {
